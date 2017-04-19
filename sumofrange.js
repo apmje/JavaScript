9fea302e4a4cc56d0e576c0f42678dd8a64529d5
apmje <apmje1@gmail.com>
2017-04-19 17:21:45 +0100

    Started Data Structures exercises

---
+++ sumofrange.js
@@ -0,0 +1,16 @@
+// Create range function with two arguments start and end that rturns an array containing all numbers from the start to the end.
+// Create a sum function with accecpts an array of numbers and returns the total sum of the numbers.
+// Run console.log(sum(range(1, 10))); and result should be 55.
+// Change range function to take a third argument, 'step', which increments the array numbers by it's value.
+// Ensure it works the same as before if no step argument is defined, ensure it works going backwards (negative step value)
+
+function range() {
+
+}
+
+function sum() {
+
+}
+
+console.log(sum(range(1, 10)));
+   
