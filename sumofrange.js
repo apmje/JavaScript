e9c853164a8967b7b131dec0a46ae3b5e122a7ac
Anthony McCormick <apmje1@gmail.com>
2017-04-19 23:24:39 +0100

    Completed The Sum of a Range exercise

--- sumofrange.js
+++ sumofrange.js
@@ -4,13 +4,31 @@
 // Change range function to take a third argument, 'step', which increments the array numbers by it's value.
 // Ensure it works the same as before if no step argument is defined, ensure it works going backwards (negative step value)
 
-function range() {
-
+function range(start, end, step) {
+    var sumArray = [];
+    if (start > end) {
+        for (var i = start; i >= end; i += step) {
+            sumArray.push(i);
+        }
+    } else {
+        for (var i = start; i <= end; i += step) {
+            if (step === undefined)
+                step = 1;
+            sumArray.push(i);
+        }
+    }
+    return sumArray;
 }
 
-function sum() {
-
+function sum(sumArray) {
+    var sumTotal = 0;
+    for (var i = 0; i < sumArray.length; i++) {
+        sumTotal += sumArray[i];
+    }
+    return sumTotal;
 }
 
+console.log(range(1, 10));
+console.log(range(1, 50, 2));
+console.log(range(5, 2, -1));
 console.log(sum(range(1, 10)));
-   
