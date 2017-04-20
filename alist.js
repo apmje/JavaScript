3cda7d1176091f3604ea560d713e4ab1039757f7
apmje <apmje1@gmail.com>
2017-04-20 17:34:42 +0100

    Reverse Array complete, tough.

---
+++ alist.js
@@ -0,0 +1,25 @@
+// Create arrayToList function which build up an object like the one in the example. 1 > 2 > 3
+// Create listToArray function that creates an array from one of the lists.
+// Create 'helper' functions, prepend which takes an element and a list creates a new list that adds the element to the front &
+// create nth function which takes a list and a number and returns the element at the given position in the list or undefined if no element exists. This should be recursive (calls itself)
+
+
+
+function arrayToList(array) {
+  
+}
+
+// function listToArray(list) {
+
+// }
+
+
+
+console.log(arrayToList([10, 20]));
+// // → {value: 10, rest: {value: 20, rest: null}}
+// console.log(listToArray(arrayToList([10, 20, 30])));
+// // → [10, 20, 30]
+// console.log(prepend(10, prepend(20, null)));
+// // → {value: 10, rest: {value: 20, rest: null}}
+// console.log(nth(arrayToList([10, 20, 30]), 1));
+// // → 20
--- reversearray.js
+++ reversearray.js
@@ -2,24 +2,25 @@
 // Create reverseArrayInPlace function which accepts an array as an argument and reverses it's elements within the same array. - Side Effect
 
 function reverseArray(array) {
-
-
+    var newArray = [];
+    for (var i = array.length - 1; i >= 0; i--) {
+        newArray.push(array[i]);
+    }
+    return newArray;
 }
 
 function reverseArrayInPlace(array) {
-
-
+    for (var i = 0; i < Math.floor(array.length / 2); i++) { // Loops from 0, incrementing by 1 until we reach half way through the array 
+        (array.length / 2 = 8 / 2 = 4)                       // and then increases i by 1, pushing it closer to the middle. If a number is odd, Math.floor will round the halfed value down.
+        var old = array[i];                                  // Puts current value i.e. array[0] = A, array[1] = B into variable old.
+        array[i] = array[array.length - 1 - i];              // Replaces the content of array from above with the last value of an array minus the i variable, which causes it to move in from end to the middle.
+        array[array.length - 1 - i] = old;                   // Replaces the last array from the end with the value in old, which is from the start.
+    }
+    return array;
 }
-
-
-
-
-console.log(reverseArray(["A", "B", "C"]));
+console.log(reverseArray(["A", "B", "C", "D"]));
 // → ["C", "B", "A"];
-
-
-
-var arrayValue = [1, 2, 3, 4, 5];
+var arrayValue = ["A", "B", "C", "D", "E", "F", "G", "H"];
 reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
 // → [5, 4, 3, 2, 1]
