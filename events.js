8d132b0022baa74ecb3c38dc4085b6098218bc4e
apmje <apmje1@gmail.com>
2017-05-18 15:30:07 +0100

    Racers complete

--- events.js
+++ events.js
@@ -3,10 +3,61 @@ h1.innerHTML = 'Hello World!';
 
 const section = document.querySelector('section');
 const selected = document.querySelector('.selected')
-section.addEventListener('mouseover', function(e) {
-   selected.innerText = e.target.className.toUpperCase(); 
+section.addEventListener('mouseover', e => {
+    selected.innerText = e.target.className.toUpperCase();
 });
 
 const purpleDiv = document.createElement('div');
 purpleDiv.className = 'purple';
 section.appendChild(purpleDiv);
+
+// let random = () => Math.floor(Math.random() * 100);
+
+// function racer(car) {
+//     let distance = random();
+//     let timedID = setInterval(() => {
+//         distance += random();
+//         car.style.marginLeft = `${distance}px`;
+//         console.log(window.innerWidth)
+//         if (distance >= window.innerWidth) {
+//             alert(`Winner!`);
+//         }
+//     }, 100);
+// };
+
+// button.addEventListener('click', function () {
+//     racer(car1);
+//     racer(car2);
+// });
+
+
+const car1 = document.querySelector('.car1');
+const car2 = document.querySelector('.car2');
+const button = document.querySelector('button');
+
+function racer(car) {
+    car.style.marginLeft = parseInt(car.style.marginLeft) + Math.floor(Math.random() * 100) + 'px';
+    console.log(car);
+    if (parseInt(car.style.marginLeft) >= window.innerWidth) {
+        alert(`${car.className.toUpperCase()} wins!`);
+        reset();
+    }
+};
+
+function reset(car) {
+    car1.style.marginLeft = 0;
+    car2.style.marginLeft = 0;
+    clearInterval(car1timer);
+    clearInterval(car2timer);
+}
+
+button.addEventListener('click', function () {
+    car1.style.marginLeft = 0;
+    car2.style.marginLeft = 0;
+    let car1timer = setInterval(function () {
+        racer(car1)
+    }, 100);
+    let car2timer = setInterval(function () {
+        racer(car2)
+    }, 100);
+});
--- nested.js
+++ nested.js
@@ -1,276 +1,276 @@
-// var instructorData = {
-//     name: "Elie",
-//     additionalData: {
-//         instructor: true,
-//         favoriteHobbies: ["Playing Cello", "Tennis", "Coding"],
-//         moreDetails: {
-//             favoriteBasketballTeam: "New York Knicks",
-//             numberOfSiblings: 3,
-//             isYoungest: true,
-//             hometown: {
-//                 city: "West Orange",
-//                 state: "NJ",
-//             },
-//             citiesLivedIn: ["Seattle", "Providence", "New York"]
-//         }
+var instructorData = {
+    name: "Elie",
+    additionalData: {
+        instructor: true,
+        favoriteHobbies: ["Playing Cello", "Tennis", "Coding"],
+        moreDetails: {
+            favoriteBasketballTeam: "New York Knicks",
+            numberOfSiblings: 3,
+            isYoungest: true,
+            hometown: {
+                city: "West Orange",
+                state: "NJ",
+            },
+            citiesLivedIn: ["Seattle", "Providence", "New York"]
+        }
+    }
+};
+
+// Loop over iterable array and log the values using for...of
+// function displayCities() {
+//     const citiesArr = instructorData.additionalData.moreDetails.citiesLivedIn;
+//     for (let value of citiesArr) {
+//         console.log(value);
 //     }
-// };
+// }
 
-// // Loop over iterable array and log the values using for...of
-// // function displayCities() {
-// //     const citiesArr = instructorData.additionalData.moreDetails.citiesLivedIn;
-// //     for (let value of citiesArr) {
-// //         console.log(value);
-// //     }
-// // }
+// displayCities();
 
-// // displayCities();
+// // Loop over values of a specific nested object
+// function displayHometownData() {
+//     const hometown = instructorData.additionalData.moreDetails.hometown;
+//     for(let key in hometown)
+//             console.log(hometown[key]);
+// }
 
-// // // Loop over values of a specific nested object
-// // function displayHometownData() {
-// //     const hometown = instructorData.additionalData.moreDetails.hometown;
-// //     for(let key in hometown)
-// //             console.log(hometown[key]);
-// // }
+// displayHometownData();
 
-// // displayHometownData();
-
-// // function addDetail(key, value) {
-// //     const moredetails = instructorData.additionalData.moreDetails;
-// //     moredetails[key] = value;
-// //     return moredetails;
-// // }
-
-// // console.log(addDetail("isHilarious", true));
-// // console.log(addDetail("previousApartments", ["Mission", "North Beach", "Nob Hill"]));
-
-// function removeDetail(key) {
+// function addDetail(key, value) {
 //     const moredetails = instructorData.additionalData.moreDetails;
-//     delete moredetails[key];
+//     moredetails[key] = value;
 //     return moredetails;
-// };
-
-// removeDetail('citiesLivedIn');
-// console.log(removeDetail('citiesLivedIn'));
-// /*
-// {
-//     favoriteBasketballTeam: "New York Knicks",
-//     numberOfSiblings: 3,
-//     isYoungest: true,
-//     hometown: {
-//         city: "West Orange",
-//         state: "NJ",
-//     }
-// }
-// */
-// removeDetail('hometown');
-// console.log(removeDetail('hometown'));
-// /*
-// {
-//     favoriteBasketballTeam: "New York Knicks",
-//     numberOfSiblings: 3,
-//     isYoungest: true
-// }
-// */
-// removeDetail('favoriteBasketballTeam');
-// console.log(removeDetail('favoriteBasketballTeam'));
-// /*
-// {
-//     numberOfSiblings: 3,
-//     isYoungest: true
-// }
-// */
-
-// var nestedData = {
-//     innerData: {
-//         order: ["first", "second", "third"],
-//         snacks: ["chips", "fruit", "crackers"],
-//         numberData: {
-//             primeNumbers: [2, 3, 5, 7, 11],
-//             fibonnaci: [1, 1, 2, 3, 5, 8, 13]
-//         },
-//         addSnack: function (snack) {
-//             this.snacks.push(snack);
-//             return this;
-//         }
-//     }
 // }
 
-// function logPrimeNumbers() {
-//     const obj = nestedData.innerData.numberData.primeNumbers;
-//     for (let key of obj) {
-//         console.log(key);
-//     }
-// };
+// console.log(addDetail("isHilarious", true));
+// console.log(addDetail("previousApartments", ["Mission", "North Beach", "Nob Hill"]));
 
-// // logPrimeNumbers();
+function removeDetail(key) {
+    const moredetails = instructorData.additionalData.moreDetails;
+    delete moredetails[key];
+    return moredetails;
+};
 
-// function logFibonnaciNumbers() {
-//     const obj = nestedData.innerData.numberData.fibonnaci;
-//     for (let key of obj) {
-//         if (key % 2 === 0) {
-//             console.log(key);
-//         }
-//     }
-// };
+removeDetail('citiesLivedIn');
+console.log(removeDetail('citiesLivedIn'));
+/*
+{
+    favoriteBasketballTeam: "New York Knicks",
+    numberOfSiblings: 3,
+    isYoungest: true,
+    hometown: {
+        city: "West Orange",
+        state: "NJ",
+    }
+}
+*/
+removeDetail('hometown');
+console.log(removeDetail('hometown'));
+/*
+{
+    favoriteBasketballTeam: "New York Knicks",
+    numberOfSiblings: 3,
+    isYoungest: true
+}
+*/
+removeDetail('favoriteBasketballTeam');
+console.log(removeDetail('favoriteBasketballTeam'));
+/*
+{
+    numberOfSiblings: 3,
+    isYoungest: true
+}
+*/
 
-// console.log(logFibonnaciNumbers());
+var nestedData = {
+    innerData: {
+        order: ["first", "second", "third"],
+        snacks: ["chips", "fruit", "crackers"],
+        numberData: {
+            primeNumbers: [2, 3, 5, 7, 11],
+            fibonnaci: [1, 1, 2, 3, 5, 8, 13]
+        },
+        addSnack: function (snack) {
+            this.snacks.push(snack);
+            return this;
+        }
+    }
+}
 
-// console.log(nestedData.innerData.order[1]);
+function logPrimeNumbers() {
+    const obj = nestedData.innerData.numberData.primeNumbers;
+    for (let key of obj) {
+        console.log(key);
+    }
+};
 
-// // nestedData.innerData.addSnack('chocolate');
-// // console.log(nestedData.innerData.snacks);
+// logPrimeNumbers();
 
-// var nestedObject = {
-//   speakers: [{name:"Elie"},{name:"Tim"},{name:"Matt"}],
-//   data: {
-//     continents: {
-//       europe: {
-//         countries: {
-//           switzerland: {
-//             capital: "Bern",
-//             population: 8000000
-//           }
-//         }
-//       }
-//     },
-//     languages: {
-//       spanish: {
-//           hello: "Hola"
-//       },
-//       french: {
-//           hello: "Bonjour"
-//       }
-//     }
-//   }
-// }
+function logFibonnaciNumbers() {
+    const obj = nestedData.innerData.numberData.fibonnaci;
+    for (let key of obj) {
+        if (key % 2 === 0) {
+            console.log(key);
+        }
+    }
+};
 
+console.log(logFibonnaciNumbers());
 
-// function addSpeaker(speaker) {
-//     const speakers = nestedObject.speakers;
-//     speakers.push({name: speaker});
-// } ;
+console.log(nestedData.innerData.order[1]);
 
-// addSpeaker('Anthony')
-// console.log(nestedObject.speakers);
+// nestedData.innerData.addSnack('chocolate');
+// console.log(nestedData.innerData.snacks);
 
-// function addLanguage (key, value) {
-//     const obj = nestedObject.data.languages;
-//     obj[key] = {hello: value};
-// };
-
-// addLanguage('english', 'Hello');
-// console.log(nestedObject.data.languages);
-
-// function addCountry (key, capital, pop) {
-//     const obj = nestedObject.data.continents.europe.countries;
-//     obj[key] = {capital: capital, 
-//                 population: pop};
-// };
-
-// addCountry('uk', 'London', '10000000')
-// console.log(nestedObject.data.continents.europe.countries);
-
-
-// var nestedArr = [[1,2,3], [4,5,6], [7,8,9,10,11,12]];
-
-// for (i = 0; i < nestedArr.length; i++) {
-//     for (j = 0; j < nestedArr[i].length; j++) {
-//         console.log(nestedArr[i][j])
-//     }
-// }
-
-
-// var nestedArr = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8],
-//   [9, 10, 11, 12]
-// ];
-
-// function printEvens() {
-//   for (i = 0; i < nestedArr.length; i++) {
-//     for (j = 0; j < nestedArr[i].length; j++) {
-//       if (nestedArr[i][j] % 2 === 0) {
-//         console.log(nestedArr[i][j]);
-//       }
-//     }
-//   }
-// };
-
-// console.log(printEvens());
-
-// var nestedArr = [
-//   [
-//     [1, 2],
-//     [3, 4]
-//   ],
-//   [
-//     [5, 6]
-//   ]
-// ];
-
-// function sumTotal() {
-//   let totalArr = [], total = 0;
-//   for (let i = 0; i < nestedArr.length; i++) {
-//     for (let j = 0; j < nestedArr[i].length; j++) {
-//       totalArr.push(nestedArr[i][j]);
-//     }
-//   }
-//   for (let k = 0; k < totalArr.length; k++) {
-//     for (let m = 0; m < totalArr[k].length; m++) {
-//       total += totalArr[k][m];
-//     }
-//   }
-//   return total;
-// };
-
-// console.log(sumTotal());
-
-// function rotate(arr, num) {
-//   for (i = 0; i < num; i++)
-//     arr.unshift(arr.pop());
-//     return arr;
-// };
-
-// console.log(rotate([1,2,3], 1)); // [3,1,2]
-// console.log(rotate([1,2,3], 2)); // [2,3,1]
-// console.log(rotate([1,2,3], 3)); // [1,2,3]
+var nestedObject = {
+  speakers: [{name:"Elie"},{name:"Tim"},{name:"Matt"}],
+  data: {
+    continents: {
+      europe: {
+        countries: {
+          switzerland: {
+            capital: "Bern",
+            population: 8000000
+          }
+        }
+      }
+    },
+    languages: {
+      spanish: {
+          hello: "Hola"
+      },
+      french: {
+          hello: "Bonjour"
+      }
+    }
+  }
+}
 
 
-// function makeXOGrid(row, col) {
-//   let arr = [],
-//     start = true;
+function addSpeaker(speaker) {
+    const speakers = nestedObject.speakers;
+    speakers.push({name: speaker});
+} ;
 
-//   for (let i = 0; i < row; i++) {
-//     let colArr = []
-//     for (let j = 0; j < col; j++) {
+addSpeaker('Anthony')
+console.log(nestedObject.speakers);
 
-//       if (start)
-//         colArr.push('X');
-//       else 
-//         colArr.push('O');
+function addLanguage (key, value) {
+    const obj = nestedObject.data.languages;
+    obj[key] = {hello: value};
+};
+
+addLanguage('english', 'Hello');
+console.log(nestedObject.data.languages);
+
+function addCountry (key, capital, pop) {
+    const obj = nestedObject.data.continents.europe.countries;
+    obj[key] = {capital: capital, 
+                population: pop};
+};
+
+addCountry('uk', 'London', '10000000')
+console.log(nestedObject.data.continents.europe.countries);
+
+
+var nestedArr = [[1,2,3], [4,5,6], [7,8,9,10,11,12]];
+
+for (i = 0; i < nestedArr.length; i++) {
+    for (j = 0; j < nestedArr[i].length; j++) {
+        console.log(nestedArr[i][j])
+    }
+}
+
+
+var nestedArr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8],
+  [9, 10, 11, 12]
+];
+
+function printEvens() {
+  for (i = 0; i < nestedArr.length; i++) {
+    for (j = 0; j < nestedArr[i].length; j++) {
+      if (nestedArr[i][j] % 2 === 0) {
+        console.log(nestedArr[i][j]);
+      }
+    }
+  }
+};
+
+console.log(printEvens());
+
+var nestedArr = [
+  [
+    [1, 2],
+    [3, 4]
+  ],
+  [
+    [5, 6]
+  ]
+];
+
+function sumTotal() {
+  let totalArr = [], total = 0;
+  for (let i = 0; i < nestedArr.length; i++) {
+    for (let j = 0; j < nestedArr[i].length; j++) {
+      totalArr.push(nestedArr[i][j]);
+    }
+  }
+  for (let k = 0; k < totalArr.length; k++) {
+    for (let m = 0; m < totalArr[k].length; m++) {
+      total += totalArr[k][m];
+    }
+  }
+  return total;
+};
+
+console.log(sumTotal());
+
+function rotate(arr, num) {
+  for (i = 0; i < num; i++)
+    arr.unshift(arr.pop());
+    return arr;
+};
+
+console.log(rotate([1,2,3], 1)); // [3,1,2]
+console.log(rotate([1,2,3], 2)); // [2,3,1]
+console.log(rotate([1,2,3], 3)); // [1,2,3]
+
+
+function makeXOGrid(row, col) {
+  let arr = [],
+    start = true;
+
+  for (let i = 0; i < row; i++) {
+    let colArr = []
+    for (let j = 0; j < col; j++) {
+
+      if (start)
+        colArr.push('X');
+      else 
+        colArr.push('O');
       
-//       start = !start;
-//     }
-//     arr.push(colArr);
-//   }
-//   return arr;
-// };
+      start = !start;
+    }
+    arr.push(colArr);
+  }
+  return arr;
+};
 
-// console.log(makeXOGrid(1, 4));
+console.log(makeXOGrid(1, 4));
 
-// /*/
-// [["X","O","X","O"]]
-// /*/
+/*/
+[["X","O","X","O"]]
+/*/
 
-// console.log(makeXOGrid(3, 2));
+console.log(makeXOGrid(3, 2));
 
-// /*/
-// [["X","O"],["X","O"],["X","O"]]
-// /*/
+/*/
+[["X","O"],["X","O"],["X","O"]]
+/*/
 
-// console.log(makeXOGrid(3, 3));
-// /*/
-// [["X","O","X"],["O","X","O"],["X","O","X"]]
-// /*/
+console.log(makeXOGrid(3, 3));
+/*/
+[["X","O","X"],["O","X","O"],["X","O","X"]]
+/*/
