25b6ebda83cc7a0aeea5d0bc56c0fcb68417a8a9
apmje <apmje1@gmail.com>
2017-05-19 17:25:33 +0100

    Finished Rithm todo list part 1

--- todo.js
+++ todo.js
@@ -2,26 +2,32 @@ const form = document.querySelector('#newTodoForm');
 const list = document.querySelector('#todoList')
 const task = document.querySelector('#task');
 
+// Add new Todo item
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-
-    const li = document.createElement('li');
-    list.appendChild(li)
-    li.innerHTML = task.value;
-
-    task.value = "";
-
+    const removeButton = document.createElement('button');
+    removeButton.innerHTML = 'Remove';
+    if (task.value === '') {
+        alert('Please enter a Todo item');
+    } else {
+        const li = document.createElement('li');
+        list.appendChild(li)
+        list.appendChild(removeButton);
+        li.innerHTML = task.value;
+        task.value = "";
+    }
 });
 
-list.addEventListener('mouseover', function (event) {
-    event.target.style.textDecoration = 'line-through';
-});
-
-list.addEventListener('mouseout', function (event) {
-    event.target.style.textDecoration = 'none';
-});
-
-
+// Line-through text if selected.
 list.addEventListener('click', function (event) {
-    list.removeChild(event.target);
+    if (event.target.tagName.toLowerCase() === 'li') {
+        if (!event.target.style.textDecoration) {
+            event.target.style.textDecoration = 'line-through';
+        } else if (event.target.style.textDecoration === 'line-through') {
+            event.target.style.cssText = null;
+        }
+    } else if (event.target.tagName.toLowerCase() === 'button') {
+        event.target.previousElementSibling.remove()
+        event.target.remove();
+    }
 });
