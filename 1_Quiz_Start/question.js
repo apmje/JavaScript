9888004f6160b7e7e357761f135afd9e9fb36abd
Anthony McCormick <apmje1@gmail.com>
2017-05-19 22:03:22 +0100

    Finished OO treehouse quiz, wasn't very modular O.o

--- 1_Quiz_Start/question.js
+++ 1_Quiz_Start/question.js
@@ -12,35 +12,46 @@ const question3 = new Question("Who directed Wild Strawberries, Fanny and Alexan
 const questionArray = [question1, question2, question3];
 
 const questionElement = document.querySelector('#question');
-questionElement.textContent = questionArray[current].question;
 const scoreElement = document.querySelector('#score');
-scoreElement.textContent = `Current score: ${score}`;
 const choice0 = document.querySelector('#choice0');
-choice0.textContent = questionArray[current].answers[0];
 const choice1 = document.querySelector('#choice1');
-choice1.textContent = questionArray[current].answers[1];
 const progress = document.querySelector('#progress');
-progress.textContent = `Question ${current + 1} of ${questionArray.length}`
+nextQuestion();
 
+function nextQuestion() {
+    if (current === questionArray.length) {
+        scoreElement.textContent = `Quiz over! You're final score is ${score}`;
+        choice0Btn.disabled = true;
+        choice1Btn.disabled = true;
+    } else {
+        questionElement.textContent = questionArray[current].question;
+        scoreElement.textContent = `Current score: ${score}`;
+        choice0.textContent = questionArray[current].answers[0];
+        choice1.textContent = questionArray[current].answers[1];
+        progress.textContent = `Question ${current + 1} of ${questionArray.length}`
+    }
+}
 
-const chocie0Btn = document.querySelector('#guess0');
+const choice0Btn = document.querySelector('#guess0');
 choice0Btn.addEventListener('click', (e) => {
-    if (questionArray[current].answers[0] === questionArray[current].correctAnswer) {
+    if (0 === questionArray[current].correctAnswer) {
         score++;
         current++;
+        nextQuestion();
     } else {
         current++;
+        nextQuestion();
     };
-})
+});
+
 const choice1Btn = document.querySelector('#guess1');
 choice1Btn.addEventListener('click', (e) => {
-    if (questionArray[current].answers[1] === questionArray[current].correctAnswer) {
+    if (1 === questionArray[current].correctAnswer) {
         score++;
         current++;
+        nextQuestion();
     } else {
         current++;
+        nextQuestion();
     };
 })
-
-
-
