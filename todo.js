75964b5e625f68b40b9dad322b438d71f0484498
apmje <apmje1@gmail.com>
2017-05-22 12:53:57 +0100

    Part 4 for Rithm todo list. Adding list items to local storage, WIP

--- todo.js
+++ todo.js
@@ -1,20 +1,25 @@
 const form = document.querySelector('#newTodoForm');
 const list = document.querySelector('#todoList')
 const task = document.querySelector('#task');
+const todoNode = document.getElementsByTagName('li');
+
+function createNewListItem(value) {
+    const removeButton = document.createElement('button');
+    const li = document.createElement('li');
+    removeButton.innerHTML = 'Remove';
+    list.appendChild(li)
+    list.appendChild(removeButton);
+    li.innerHTML = value;
+};
 
 // Add new Todo item
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    const removeButton = document.createElement('button');
-    removeButton.innerHTML = 'Remove';
     if (task.value === '') {
         alert('Please enter a Todo item');
     } else {
-        const li = document.createElement('li');
-        list.appendChild(li)
-        list.appendChild(removeButton);
-        li.innerHTML = task.value;
-        task.value = "";
+        createNewListItem(task.value);
+        task.value = '';
     }
 });
 
@@ -29,5 +34,18 @@ list.addEventListener('click', function (event) {
     } else if (event.target.tagName.toLowerCase() === 'button') {
         event.target.previousElementSibling.remove()
         event.target.remove();
+        
     }
 });
+
+function populateStorage() {
+    for (let i = 0; i < todoNode.length; i++) {
+        localStorage.setItem('listItem' + i, todoNode[i].textContent);
+    }
+};
+
+function init() {
+    for (let key in localStorage) {
+        createNewListItem(localStorage.getItem(key));
+    }
+};
