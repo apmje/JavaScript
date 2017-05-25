062ddb21bffc0c2a9a69696759b1a1e134e28854
apmje <apmje1@gmail.com>
2017-05-25 10:04:35 +0100

    Storing object with todo item succcessfully to local storage and retrieving it back with the state!

--- Rithm/todo.js
+++ Rithm/todo.js
@@ -4,6 +4,7 @@ const task = document.querySelector('#task');
 const todoNode = document.getElementsByTagName('li');
 let storArr = JSON.parse(localStorage.getItem('listItems')) || [];
 
+
 // Function to create a new list item in the UI
 function createNewListItem(value) {
     const removeButton = document.createElement('button');
@@ -12,6 +13,7 @@ function createNewListItem(value) {
     list.appendChild(li)
     list.appendChild(removeButton);
     li.innerHTML = value;
+    return li;
 };
 
 // Add new Todo item using createNewListItem function and pushes the list item to the array
@@ -21,8 +23,11 @@ form.addEventListener("submit", function (event) {
         alert('Please enter a Todo item');
     } else {
         createNewListItem(task.value);
-        storArr.push({task: task.value, isComplete: false});
-        task.value = '';
+        storArr.push({
+            task: task.value,
+            isComplete: false
+        });
+        form.reset();
     }
 });
 
@@ -31,13 +36,22 @@ list.addEventListener('click', function (event) {
     if (event.target.tagName.toLowerCase() === 'li') {
         if (!event.target.style.textDecoration) {
             event.target.style.textDecoration = 'line-through';
+            for (let i = 0; i < storArr.length; i++)
+                if (event.target.innerHTML === storArr[i].task) {
+                    storArr[i].isComplete = true;
+                }
         } else if (event.target.style.textDecoration === 'line-through') {
             event.target.style.cssText = null;
+            for (let i = 0; i < storArr.length; i++)
+                if (event.target.innerHTML === storArr[i].task) {
+                    storArr[i].isComplete = false;
+                }
         }
     } else if (event.target.tagName.toLowerCase() === 'button') {
-        if (storArr.includes(event.target.previousElementSibling.innerHTML)) {
-            let i = storArr.indexOf(event.target.previousElementSibling.innerHTML)
-            storArr.splice(i, 1);
+        for (let i = 0; i < storArr.length; i++) {
+            if (event.target.previousElementSibling.innerHTML === storArr[i].task) {
+                storArr.splice(i, 1);
+            }
         }
         event.target.previousElementSibling.remove();
         event.target.remove();
@@ -46,14 +60,14 @@ list.addEventListener('click', function (event) {
 
 // Push all array items to local storage upon window close
 window.onbeforeunload = function populateStorage() {
-        localStorage.setItem("listItems", JSON.stringify(storArr));
+    localStorage.setItem("listItems", JSON.stringify(storArr));
 };
 
 // Recreate all items in the list when window reopens
 window.onload = function init() {
     for (let value of storArr) {
         let todo = createNewListItem(value.task);
-        if (value.isComplete ===  true) {
+        if (value.isComplete) {
             todo.style.textDecoration = 'line-through';
         }
     }
